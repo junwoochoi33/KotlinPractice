52fe23e201b78a17730da79482d077640cf22bb0
jwchoi <jwchoi@wisenut.co.kr>
2025-08-11 14:41:41 +0900

    reaching outer class from inner class

---
+++ src/domain/model/Base.kt
@@ -0,0 +1,26 @@
+package domain.model
+
+open class Base {
+    open val x : Int = 1
+    open fun f() = println("Base function f() called, x = $x")
+}
+
+class Child : Base() {
+    override val x: Int = super.x + 1
+    override fun f() = println("Child function f() called, x = $x")
+
+    inner class Inside {
+        fun f() = println("Inside class function f() called, x = $x")
+        fun test() {
+            f()
+            Child().f()
+            super@Child.f()
+            println("Inside class test() called, x = $x, super.x = ${super@Child.x}")
+        }
+    }
+}
+
+fun main() {
+    val c1 = Child()
+    c1.Inside().test()
+}
